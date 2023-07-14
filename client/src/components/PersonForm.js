ef91547ba9c6275b5fe25077b8dc1cffdeeb4125
Matt Mottle <mattmottle1@gmail.com>
2023-07-14 07:47:01 -0700

    first commit

---
+++ client/src/components/PersonForm.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+const PersonForm = (props) => {
+    const {people, setPeople} = props; //this is new
+    const [firstName, setFirstName] = useState(""); 
+    const [lastName, setLastName] = useState("");
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:8000/api/people', {
+            firstName,    
+            lastName   
+        })
+            .then(res=>{
+                console.log(res); 
+                console.log(res.data);
+                // we will update the lifted state of our people array 
+                //    to include the current value in state plus the single 
+                //    new object created and returned from our post request. 
+                setPeople([...people, res.data]); //this is new
+            })
+            .catch(err=>console.log(err))
+    }       // **** Our return/form will remain unchanged! 
+    
+    return (
+        <form onSubmit={onSubmitHandler}>
+            <p>
+                <label>First Name</label><br/>
+                
+                <input type="text" onChange = {(e)=>setFirstName(e.target.value)}/>
+            </p>
+            <p>
+                <label>Last Name</label><br/>
+                <input type="text" onChange = {(e)=>setLastName(e.target.value)}/>
+            </p>
+            <input type="submit"/>
+        </form>
+    )
+}
+export default PersonForm;
---
+++ server/controllers/person.controller.js
@@ -0,0 +1,45 @@
+const Person = require('../models/person.model');
+
+module.exports.index = (req, res) => {
+    res.json({
+        message: "Hello World"
+    });
+}
+
+module.exports.createPerson = (req, res) => {
+    // Mongoose's "create" method is run using our Person model to add a new person to our db's person collection.
+    // request.body will contain something like {firstName: "Billy", lastName: "Washington"} from the client
+    Person.create(req.body) //This will use whatever the body of the client's request sends over
+        .then(person => res.json(person))
+        .catch(err => res.json(err));
+}
+
+module.exports.getAllPeople = (req, res) => {
+    Person.find({})
+        .then(persons => {
+            console.log(persons); //console logs are optional, but they are highly recommended for troubleshooting!
+            res.json(persons);
+        })
+        .catch(err => {
+            console.log(err)
+            res.json(err)
+        })
+}
+
+module.exports.getPerson = (req, res) => {
+    Person.findOne({_id:req.params.id})
+        .then(person => res.json(person))
+        .catch(err => res.json(err));
+}
+
+module.exports.updatePerson = (req, res) => {
+    Person.findOneAndUpdate({_id: req.params.id}, req.body, {new:true})
+        .then(updatedPerson => res.json(updatedPerson))
+        .catch(err => res.json(err))
+}
+
+module.exports.deletePerson = (req, res) => {
+    Person.deleteOne({ _id: req.params.id }) //note: "id" here MUST match id in corresponding route
+        .then(deleteConfirmation => res.json(deleteConfirmation))
+        .catch(err => res.json(err))
+}
